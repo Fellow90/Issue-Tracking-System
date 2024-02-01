c152ab2268281fb50b46c8b5915054a4e2d9bddc
Fellow90 <poudelnabraaz99@gmail.com>
2024-02-01 09:53:24 +0545

    changed from sqlite to postgres

--- issueProject/settings.py
+++ issueProject/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'issueProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'nabin_db',
+        'USER' : 'nabindai',
+        'PASSWORD' : 'nabindai',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+}
 }
 
 
